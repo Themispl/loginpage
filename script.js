2fb2d288f18874d1eb138ee3d213d812fdd375db
EFTHYMIOS ANTONIOS PYLARINOS <124589960+Themispl@users.noreply.github.com>
2023-06-13 20:30:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const wrapper = document.querySelector('.formBox');
+
+const login = document.querySelector('.btnLogin-popup');
+
+
+login.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+  });
+  
