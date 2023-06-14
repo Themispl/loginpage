9a1c664975a96223c9f77f41c52db387942de707
EFTHYMIOS ANTONIOS PYLARINOS <124589960+Themispl@users.noreply.github.com>
2023-06-14 16:52:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ const wrapper = document.querySelector('.formBox');
 
 const login = document.querySelector('.btnLogin-popup');
 
+const close = document.querySelector('.icon-close');
 
 login.addEventListener('click', ()=> {
     wrapper.classList.add('active-popup');
+  });
+  close.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
   });
   
